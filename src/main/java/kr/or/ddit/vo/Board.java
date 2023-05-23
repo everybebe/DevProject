c32bb381f5b16bc650eea28f737bccd3382a0e3a
kimmin <ksd2521@nate.com>
2023-05-23 16:29:18 +0900

    0523 Spring Date 타입 처리까지 완료

---
+++ src/main/java/kr/or/ddit/vo/Board.java
@@ -0,0 +1,13 @@
+package kr.or.ddit.vo;
+
+import lombok.Data;
+
+@Data
+public class Board {
+	private int boardNo;
+	private String title;
+	private String content;
+	private String writer;
+}
+
+
